96625514f6b5736336edfdedc43d2e714d075109
dglttr <d.glatter@outlook.com>
2021-01-12 17:26:43 +0100

    [DEL] Remove inline execution

--- regression_corr_analysis.py
+++ regression_corr_analysis.py
@@ -1,8 +1,6 @@
 import pandas as pd
 import statsmodels.api as sm
 
-from utils import load_data
-
 
 def feature_selection(df: pd.DataFrame, threshold: float = 0.6, verbose: bool = False) -> pd.DataFrame:
     """Iteratively drop the column most explained by the other columns, until threshold is reached."""
@@ -32,21 +30,3 @@ def feature_selection(df: pd.DataFrame, threshold: float = 0.6, verbose: bool = 
     print("R2 per column:\n\t", rsquare_per_col)
 
     return df
-
-
-DATA_DIRECTORY = r"C:\Users\Daniel\Documents\Studium\7. Semester (WS 2020.21)\Seminar Data Mining in der Produktion\Gruppenarbeit\Data"
-COMPONENTS = [
-    ['A_1', 'A_2', 'A_3', 'A_4', 'A_5'],
-    ['B_1', 'B_2', 'B_3', 'B_4', 'B_5'],
-    ['C_1', 'C_2', 'C_3', 'C_4', 'C_5'],
-    ['L_1', 'L_2', 'L_3', 'L_4', 'L_5', 'L_6', 'L_7', 'L_8', 'L_9', 'L_10']
-]
-
-files = load_data(DATA_DIRECTORY)
-file = pd.concat(files)
-THRESHOLD = 0.6
-
-for group in COMPONENTS:
-    feature_selection(file[group], threshold=THRESHOLD)
-
-a = 1
