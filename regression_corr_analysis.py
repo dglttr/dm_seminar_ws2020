f80810afb3e0db45ce2ad4b1557b524a1fdd5907
dglttr <d.glatter@outlook.com>
2021-01-25 12:47:54 +0100

    [ADD] Make feature_selection() return columns to include

--- regression_corr_analysis.py
+++ regression_corr_analysis.py
@@ -1,8 +1,12 @@
+from typing import List
+
 import pandas as pd
 import statsmodels.api as sm
 
+from utils import load_data
 
-def feature_selection(df: pd.DataFrame, threshold: float = 0.6, verbose: bool = False) -> pd.DataFrame:
+
+def feature_selection(df: pd.DataFrame, threshold: float = 0.6, verbose: bool = False) -> List[str]:
     """Iteratively drop the column most explained by the other columns, until threshold is reached."""
     while len(df.columns) > 1:
         rsquare_per_col = {}
@@ -26,16 +30,14 @@ def feature_selection(df: pd.DataFrame, threshold: float = 0.6, verbose: bool = 
             print(f"Dropped col {col_with_max_r2} with R2 = {max_rsquared}")
             print("R2 per column:\n\t", rsquare_per_col)
 
-    print(f"Remaining columns: {list(df.columns)}")
-    print("R2 per column:\n\t", rsquare_per_col)
+    if verbose:
+        print(f"Remaining columns: {list(df.columns)}")
+        print("R2 per column:\n\t", rsquare_per_col)
 
-    return df
+    return list(df.columns)
 
 
 # Apply to data
-from utils import load_data
-
-
 DATA_DIRECTORY = r""
 COMPONENTS = [
     ['A_1', 'A_2', 'A_3', 'A_4', 'A_5'],
@@ -45,9 +47,11 @@ COMPONENTS = [
 ]
 THRESHOLD = 0.6
 
-
 files = load_data(DATA_DIRECTORY)
 file = pd.concat(files)     # use all files as one
 
+cols_to_include = []
 for group in COMPONENTS:
-    feature_selection(file[group], threshold=THRESHOLD)
+    cols_to_include.extend(feature_selection(file[group], threshold=THRESHOLD))
+
+print("Column to include: ", cols_to_include)
