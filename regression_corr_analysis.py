2cd9ce2cb321157f89f82cd22db12d6abb7f0e47
dglttr <d.glatter@outlook.com>
2021-01-25 12:28:01 +0100

    [ADD] Execute feature selection

--- regression_corr_analysis.py
+++ regression_corr_analysis.py
@@ -30,3 +30,24 @@ def feature_selection(df: pd.DataFrame, threshold: float = 0.6, verbose: bool = 
     print("R2 per column:\n\t", rsquare_per_col)
 
     return df
+
+
+# Apply to data
+from utils import load_data
+
+
+DATA_DIRECTORY = r""
+COMPONENTS = [
+    ['A_1', 'A_2', 'A_3', 'A_4', 'A_5'],
+    ['B_1', 'B_2', 'B_3', 'B_4', 'B_5'],
+    ['C_1', 'C_2', 'C_3', 'C_4', 'C_5'],
+    ['L_1', 'L_2', 'L_3', 'L_4', 'L_5', 'L_6', 'L_7', 'L_8', 'L_9', 'L_10']
+]
+THRESHOLD = 0.6
+
+
+files = load_data(DATA_DIRECTORY)
+file = pd.concat(files)     # use all files as one
+
+for group in COMPONENTS:
+    feature_selection(file[group], threshold=THRESHOLD)
